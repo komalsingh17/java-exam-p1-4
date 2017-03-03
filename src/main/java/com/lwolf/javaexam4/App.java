28b92dc0720e94e38dae01aee0113b21feaf2ab9
Komal Dhaliwal <kdhaliwal@lwolf.com>
2017-03-03 13:02:43 -0500

    Create program SumOrProduct

---
+++ src/main/java/com/lwolf/javaexam4/App.java
@@ -0,0 +1,57 @@
+package com.lwolf.javaexam4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        int n = 0, result = 0;
+        String operation = "";
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.print("Enter n(>=1): ");
+            n = Integer.parseInt(reader.readLine());
+            System.out.print("Compute\n'sum' or 'product': ");
+            operation = reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(n + "\n" + operation);
+        if (n < 1) {
+            System.out.println("n must be greater than 0");
+            return;
+        }
+        switch (operation) {
+            case "sum":
+                result = computeSum(n);
+                break;
+            case "product":
+                result = computeProduct(n);
+                break;
+            default:
+                System.out.println("Invalid operation");
+                break;
+        }
+        System.out.println(result);
+    }
+
+    private static int computeSum(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    private static int computeProduct(int n) {
+        int product = 1;
+        for (int i = 1; i <= n; i++) {
+            product *= i;
+        }
+        return product;
+    }
+
+}
